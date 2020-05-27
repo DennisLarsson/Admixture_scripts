bb8bba5cb3c6bd836598b360ca582db82da7a334
DennisLarsson <43995478+DennisLarsson@users.noreply.github.com>
2020-05-27 11:23:50 +0200

    Update plot_admixture.R

--- plot_admixture.R
+++ plot_admixture.R
@@ -1,22 +1,15 @@
-textpos <- function(x) {
-  pos <- mean(x) + 1
-  return(pos)
-}
+#Script used to plot results of admixture (or any other structure like program that outputs Q-values or similar). plots names of populations 
 
-pop<-read.table("/home/biogeoanalysis/RAD/spicatumPhylogeography/06populations/pop",as.is=T)
-group<-read.table("/home/biogeoanalysis/RAD/spicatumPhylogeography/06populations/group",as.is=T)
-admix<-t(as.matrix(read.table("/home/biogeoanalysis/RAD/spicatumPhylogeography/06populations/K4")))
-#admix<-admix[,order(pop[,1])]
-group_sorted<-group[order(group[,1]),]
-pop_sorted<-pop[order(pop[,1]),]
+admix<-t(as.matrix(read.table("/path/to/admixtureFile"))) #file has ending .Q
+pop<-read.table("/path/to/popmap",as.is=T) #population file with name of individual in right column and name of population in left column. columns separated by tab.
+#group<-read.table("/path/to/groupmap",as.is=T) #optional group file with name of individual in right column and name of group in left column. columns separated by tab.
+
+pop_sorted<-pop[order(pop[,2]),]
+#group_sorted<-group[order(group[,2]),] # uncomment if groupmap is used.
+
 h <- barplot(admix, col=c("blue", "red", "orange", "darkgreen", "purple","brown","black"), 
-             space=0, border=NA, xlab="Individuals", ylab="admixture")
-text(tapply(1:nrow(group), group[,1],mean), -0.05, unique(group_sorted[,1]), xpd=T, srt=45, adj = c(0.60,0), cex = 0.85)
-text(tapply(1:nrow(pop), pop[,1],textpos), 1.05, unique(pop_sorted[,1]), xpd=T, srt=45, adj = c(0.4,0), cex = 0.75)
-abline(v=c(31,58,106), lwd=1.5)
-abline(v=c(3,7,11,15,19,23,27,31,35,39,43,47,50,54,58,62,66,70,74,78,82,86,90,94,98,102,106,110,114,118,122,126,130,134,138), lty=2, lwd=0.5)
-
-
-#SpicGrp_sub
-#abline(v=c(7,15,23,30,34,46,62), lwd=1.5)
-#abline(v=c(4,7,11,15,19,23,27,30,34,38,42,46,50,54,58,62,66,70,74,77,81,85), lty=2, lwd=0.5)
+             space=0, border=NA, ylab="admixture")
+axis(1, tapply(1:nrow(pop), pop[,2],mean),unique(pop_sorted[,2]),las=2, cex.axis=0.6,tick = F,line = -0.8)
+#axis(3, tapply(1:nrow(group), group[,2],mean),unique(group_sorted[,2]),las=2, cex.axis=0.6,tick = F,line = -0.8) # uncomment if groupmap is used.
+abline(v=tapply(1:nrow(pop), pop[,2],max), lty=2, lwd=0.5)
+#abline(v=tapply(1:nrow(group), group[,2],max), lty=1, lwd=1) # uncomment if groupmap is used
